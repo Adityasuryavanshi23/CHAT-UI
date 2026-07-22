54dc8911bda76a05500e83558e68c443c223c74b
Aditya suryavanshi <167076950+Adityasuryavanshi23@users.noreply.github.com>
2026-07-22 19:16:58 +0530

    Remove duplicate MessageBox components
    Removed duplicate MessageBox components from layout.

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,6 @@ function App() {
       <div className="min-h-screen flex dark-gradient">
         <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
         <div className="flex-[2] bg-gray-800/10 h-screen">
-          <MessageBox />
-          <MessageBox />
           
           
         </div>
